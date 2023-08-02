985952c23e95bafd6c8c9cdf9c7ceb2dfa891f53
han8129 <110092300+han8129@users.noreply.github.com>
2023-08-02 11:11:18 +0700

    refactor ibus in singleton

--- lua/setup/ibus.lua
+++ lua/setup/ibus.lua
@@ -1,49 +1,79 @@
-local EN = 'BambooUs'
-local VI = 'Bamboo'
-local IBUS_PREVIOUS = ''
+-- Manage Ibus bamboo
+--
+-- Turn off iBus VN when enter Vim.
+-- If Ibus bamboo (VN) is set in insert mode, switch to US
+-- in normal mode.
+function IBusManager()
+    local obj = {}
 
-function IBusOff()
-    local ibus_current = vim.fn.system('ibus engine')
+    function obj.new()
+        if obj._instance then
+            return obj._instance
+        end
 
-    if ibus_current == EN then
-        return
+        obj.__index = obj
+        obj._EN = 'BambooUs'
+        obj._VI = 'Bamboo'
+        obj._previousState = 'BambooUs'
+        obj._instance = setmetatable({}, obj)
+
+        return obj._instance
     end
 
-    IBUS_PREVIOUS = ibus_current
+    vim.fn.system('iBus engine BambooUs')
 
-    vim.fn.system('ibus engine BambooUs')
-end
-
-function IBusOn()
-    local ibus_current = vim.fn.system('ibus engine')
-
-    if ibus_current == VI then
-        return
+    local function on()
+        vim.fn.system('iBus engine ' .. obj._VI)
     end
 
-    vim.fn.system('ibus engine ' .. IBUS_PREVIOUS)
+    local function off()
+        vim.fn.system('iBus engine ' .. obj._EN)
+    end
+
+    local function getCurrentState()
+        return vim.fn.system('iBus engine')
+    end
+
+    function obj.on()
+        if obj._previousState == obj._VI then
+            on()
+        end
+    end
+
+    function obj.off()
+        local state = getCurrentState()
+
+        if state == obj._EN then
+            return false
+        end
+
+        off()
+
+        obj._previousState = state
+    end
+
+    return obj
 end
 
-IBusOff()
+iBus = IBusManager().new()
 
 vim.cmd([[
 
 augroup IBusHandler
 autocmd!
-" Khôi phục ibus engine khi tìm kiếm
-autocmd CmdLineEnter [/?] silent lua IBusOn()
-autocmd CmdLineLeave [/?] silent lua IBusOff()
-autocmd CmdLineEnter \? silent lua IBusOn()
-autocmd CmdLineLeave \? silent lua IBusOff()
-" Khôi phục ibus engine khi vào insert mode
-autocmd InsertEnter * silent lua IBusOn()
-" Tắt ibus engine khi vào normal mode
-autocmd InsertLeave * silent lua IBusOff()
+" Khôi phục iBus engine khi tìm kiếm
+autocmd CmdLineEnter [/?] silent lua iBus.on()
+autocmd CmdLineLeave [/?] silent lua iBus.off()
+autocmd CmdLineEnter \? silent lua iBus.on()
+autocmd CmdLineLeave \? silent lua iBus.off()
+" Khôi phục iBus engine khi vào insert mode
+autocmd InsertEnter * silent lua iBus.on()
+" Tắt iBus engine khi vào normal mode
+autocmd InsertLeave * silent lua iBus.off()
 
-autocmd VimEnter * silent lua IBusOff()
-autocmd VimLeave * silent lua IBusOn()
+autocmd VimEnter * silent lua iBus.off()
 
-autocmd FocusGained * silent lua IBusOff()
+autocmd FocusGained * silent lua iBus.off()
 
 augroup END
 ]])
